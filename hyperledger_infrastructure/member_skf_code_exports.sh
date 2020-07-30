83421cca56e3157d672fc27de50cfd56afb9bdc4
Simon Gensler <hallo@simongensler.de>
2020-07-30 15:08:48 +0200

    Add exports for custom chaincode

---
+++ hyperledger_infrastructure/member_skf_code_exports.sh
@@ -0,0 +1,15 @@
+export MSP_PATH=/opt/home/admin-msp
+export MSP=$MEMBERID
+export ORDERER=$ORDERINGSERVICEENDPOINT
+export PEER=$PEERSERVICEENDPOINT
+export CHANNEL=testchannel
+export CAFILE=/opt/home/managedblockchain-tls-chain.pem
+export CHAINCODENAME=mycc
+export CHAINCODEVERSION=v0
+export CHAINCODEDIR=github.com/chaincode_example02/go
+
+
+# install chaincode
+#docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+    #-e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
+    #cli peer chaincode install -n ngo -l node -v v0 -p /opt/gopath/src/github.com/ngo
