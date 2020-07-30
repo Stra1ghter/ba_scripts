d15073cd02d938e52bf4d2d812df17da84f2e92a
Simon Gensler <hallo@simongensler.de>
2020-07-30 10:24:12 +0200

    Add ORDERER env var

--- hyperledger_infrastructure/general_exports.sh
+++ hyperledger_infrastructure/general_exports.sh
@@ -6,6 +6,7 @@ export VpcEndpointServiceName=com.amazonaws.eu-west-1.managedblockchain.n-rptbif
 export OrderingServiceEndpoint=orderer.n-rptbife6ujgfhkxxvmds35rg7m.managedblockchain.eu-west-1.amazonaws.com:30001
 export ORDERINGSERVICEENDPOINT=$OrderingServiceEndpoint
 export ORDERINGSERVICEENDPOINTNOPORT=${ORDERINGSERVICEENDPOINT::-6}
+export ORDERER=$OrderingServiceEndpoint
 
 export VpcEndpointServiceName=com.amazonaws.eu-west-1.managedblockchain.n-rptbife6ujgfhkxxvmds35rg7m
 export OrderingServiceEndpoint=orderer.n-rptbife6ujgfhkxxvmds35rg7m.managedblockchain.eu-west-1.amazonaws.com:30001
--- hyperledger_infrastructure/simple_channel/set_cfg_path.sh
+++ hyperledger_infrastructure/simple_channel/set_cfg_path.sh
@@ -1,4 +1,11 @@
 # fabric liest den config path für die yaml datei über FABRIC_CFG_PATH
 # da der CLI container einen mount nach home hat:
 
-export FABRIC_CFG_PATH=/home/ec2-user/ba_scripts/hyperledger_infrastructure/simple_channel/
+# aktuell per command aufgerufen
+#export FABRIC_CFG_PATH=/home/ec2-user/ba_scripts/hyperledger_infrastructure/simple_channel/
+
+# channel erzeugen
+docker exec cli configtxgen \
+-outputCreateChannelTx /opt/home/mychannel.pb \
+-profile OneOrgChannel -channelID mychannel \
+--configPath  /opt/home/ba_scripts/hyperledger_infrastructure/simple_channel
