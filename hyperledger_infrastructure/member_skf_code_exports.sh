1227bf23354a37b9f6c2ef0245bc2e1dac88d5b2
Simon Gensler <hallo@simongensler.de>
2020-07-30 15:16:14 +0200

    minor fix

--- hyperledger_infrastructure/member_skf_code_exports.sh
+++ hyperledger_infrastructure/member_skf_code_exports.sh
@@ -8,10 +8,10 @@ export CHAINCODENAME=mycc
 export CHAINCODEVERSION=v0
 export CHAINCODEDIR=github.com/chaincode_example02/go
 
-echo For examples how to install chaincode, have a look inside the ba_script
-exit
 
-# install chaincode
-docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
-    -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
-    cli peer chaincode install -n bearing_supply_chain -l node -v v0 -p /opt/home/ba_scripts/src/bearing_supply_chain.js
+
+
+echo install chaincode example:
+echo docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+ -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
+ cli peer chaincode install -n bearing_supply_chain -l node -v v0 -p /opt/home/ba_scripts/src/bearing_supply_chain.js
