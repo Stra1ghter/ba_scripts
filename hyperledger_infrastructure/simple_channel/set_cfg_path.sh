34468ea44aab1ee05e33d1efe05bd80e30c2a878
Simon Gensler <hallo@simongensler.de>
2020-07-30 10:19:49 +0200

    Add fabric config path

---
+++ hyperledger_infrastructure/simple_channel/set_cfg_path.sh
@@ -0,0 +1,4 @@
+# fabric liest den config path für die yaml datei über FABRIC_CFG_PATH
+# da der CLI container einen mount nach home hat:
+
+export FABRIC_CFG_PATH=/home/ec2-user/ba_scripts/hyperledger_infrastructure/simple_channel/
