5bd09a60a277eb27a6f61d1491d6ed01d77015c1
Simon Gensler <hallo@simongensler.de>
2020-07-30 16:52:41 +0200

    test

--- src/test.js
+++ src/test.js
@@ -1,9 +1,17 @@
-const fs = require("fs");
-eval(fs.readFileSync('bearing_supply_chain.js')+''); // '' turns the object to a string
+//const fs = require("fs");
+//eval(fs.readFileSync('bearing_supply_chain.js')+''); // '' turns the object to a string
 
-/*
-Simple setup
-init
-invoke
 
-*/
+import { Chaincode } from './bearing_supply_chain.js';
+
+const ChaincodeMockStub = require("@theledger/fabric-mock-stub")
+
+const chaincode = new MyChaincode();
+
+describe('Test MyChaincode', () => {
+    it("Should init without issues", async () => {
+        const mockStub = new ChaincodeMockStub("MyMockStub", chaincode);
+        const response = await mockStub.mockInit("tx1", []);
+        expect(response.status).to.eql(200)
+    });
+});
