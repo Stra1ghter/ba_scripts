a300cb0565be1d7703b1bf9341cc6e8b0dd84676
Simon Gensler <hallo@simongensler.de>
2020-07-31 08:49:36 +0200

    Fix export code script

--- hyperledger_infrastructure/member_skf_code_exports.sh
+++ hyperledger_infrastructure/member_skf_code_exports.sh
@@ -14,12 +14,12 @@ echo
 echo install chaincode example:
 echo docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
  -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
- cli peer chaincode install -n bearing_supply_chain -l node -v v0.2 -p /opt/home/ba_scripts/src/
+ cli peer chaincode install -n bearing_supply_chain -l node -v v1.0.1 -p /opt/home/ba_scripts/src/package/
 
 
 echo
 echo instantiate chaincode example:
 echo docker exec cli peer chaincode instantiate \
--o $ORDERER -C mychannel -n bearing_supply_chain -v v0.2 \
+-o $ORDERER -C mychannel -n bearing_supply_chain -v v1.0.1 \
 -c \'{\"Args\":[\"init\"]}\' \
 --cafile /opt/home/managedblockchain-tls-chain.pem --tls
