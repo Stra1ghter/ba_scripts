615a0c88587cf216db28b73470736ab77b04bedf
Simon Gensler <hallo@simongensler.de>
2020-07-31 09:50:55 +0200

    ...

--- hyperledger_infrastructure/member_skf_code_exports.sh
+++ hyperledger_infrastructure/member_skf_code_exports.sh
@@ -14,20 +14,30 @@ echo
 echo install chaincode example:
 echo docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
  -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
- cli peer chaincode install -n bearing_supply_chain -l node -v v1.0.1 -p /opt/home/ba_scripts/src/package/
+ cli peer chaincode install -n bearing_supply_chain -l node -v v1.0.3 -p /opt/home/ba_scripts/src/package/
 
 
 echo
 echo instantiate chaincode example:
 echo docker exec cli peer chaincode instantiate \
--o $ORDERER -C mychannel -n bearing_supply_chain -v v1.0.1 \
+-o $ORDERER -C mychannel -n bearing_supply_chain -v v1.0.3 \
 -c \'{\"Args\":[\"init\"]}\' \
 --cafile /opt/home/managedblockchain-tls-chain.pem --tls
 
 
+
+echo 
+echo package
+echo docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+    -e "CORE_PEER_ADDRESS=$PEER" -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" \
+    cli peer chaincode package -n bearing_supply_chain -l node -p /opt/home/ba_scripts/src/package/ -v v1.0.3 -s -S ccpack.out
+
+
 echo 
 echo upgradate chaincode
-echo peer chaincode upgrade -n bearing_supply_chain -v 1.0.3 \
+echo docker exec  -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+    -e "CORE_PEER_ADDRESS=$PEER" -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" \
+    cli peer chaincode upgrade -n bearing_supply_chain -v 1.0.3 \
                -c \'{\"Args\":[\"\"]}\' \
                -p /opt/home/ba_scripts/src/package/ -C mychannel \
                -o $ORDERER
