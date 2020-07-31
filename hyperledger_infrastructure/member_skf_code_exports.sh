8d8e98860a06615d0338c4e455fd5a9787a1bd4e
Simon Gensler <hallo@simongensler.de>
2020-07-31 09:54:20 +0200

    ...

--- hyperledger_infrastructure/member_skf_code_exports.sh
+++ hyperledger_infrastructure/member_skf_code_exports.sh
@@ -16,6 +16,12 @@ echo docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE
  -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
  cli peer chaincode install -n bearing_supply_chain -l node -v v1.0.3 -p /opt/home/ba_scripts/src/package/
 
+echo
+echo install chaincode package
+echo docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+ -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
+ cli peer chaincode install ccpack.out -l node
+
 
 echo
 echo instantiate chaincode example:
