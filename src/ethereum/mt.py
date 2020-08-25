d35707c41a48b86b1cce8a3c86c44ba66dac4b60
Simon Gensler <hallo@simongensler.de>
2020-08-25 14:01:55 +0200

    Finish first draft of merkle tree implementation

--- src/ethereum/mt.py
+++ src/ethereum/mt.py
@@ -6,29 +6,52 @@ from pathlib import Path
 
 class LeafNode:
     __file_hash_obj = ""
+    __merge_into = None
 
     def __init__(self, file_path):
         with file_path.open("rb") as f:
             b = bytes(f.read())
-            print(b)
-            b = b.replace(b"\r\n", b"\n") # make the newline character system independent
-            print(b)
+            b = b.replace(b"\r\n", b"\n") # make the newline character system independent to \n
             self.__file_hash_obj = sha256(b)
+            assert len(self.__file_hash_obj.hexdigest()) == 64 # 64 hex chars == 256 bit
 
     def __str__(self):
         return "LeafNode(" + self.__file_hash_obj.hexdigest() + ")"
+    def __repr__(self):
+        return "LeafNode(" + self.__file_hash_obj.hexdigest() + ")"
 
+    def digest(self):
+        return self.__file_hash_obj.digest()
 
+    def hexdigest(self):
+        return self.__file_hash_obj.hexdigest()
 
 class Node:
-    def __init__(self, child_node_l, child_node_r):
-        pass
+    __layer = 0
+    __hash_obj = ""
+
+    def __init__(self, left_child, right_child):
+        if not right_child:
+            self.__hash_obj = sha256(left_child.digest())
+        else:
+            self.__hash_obj = sha256(left_child.digest() + right_child.digest())
+
+    def __str__(self):
+        return "Node(" + self.__hash_obj.hexdigest() + ")"
+    def __repr__(self):
+        return "Node(" + self.__hash_obj.hexdigest() + ")"
+
+    def digest(self):
+        return self.__hash_obj.digest()
+
+    def hexdigest(self):
+        return self.__hash_obj.hexdigest()
 
 
 class MerkleTree:
 
-    ordered_file_list = []
-    leaf_nodes = []
+    __leaf_nodes = []
+    __merkle_root_node = None
 
     def __init__(self, data_dir):
         """
@@ -44,9 +67,35 @@ class MerkleTree:
         self.ordered_file_list.sort() # The order of files have to be the same on all systems
 
         for file_path in self.ordered_file_list:
-            self.leaf_nodes.append(LeafNode(file_path))
+            self.__leaf_nodes.append(LeafNode(file_path))
 
-        for node in self.leaf_nodes:
-            print(node)
+        self.__build_tree(),
+
+    def __build_tree(self):
+        """
+        Builds a Merkle tree over the leaf node list.
+        """
+        current_layer = self.__leaf_nodes
+        next_layer = []
+
+        left = None
+        while len(current_layer) != 1:
+            print("Layer:" + str(current_layer))
+            for i, node in enumerate(current_layer):
+                if i % 2 == 0:
+                    if i == len(current_layer) - 1:
+                        next_layer.append(Node(node, None)) # at the last index, there may only be a left node to build the next node level
+                    left = node
+                else:
+                    next_layer.append(Node(left, node))
+
+            current_layer = next_layer
+            next_layer = []
+
+        self.__merkle_root_node = current_layer[0]
+        print("Last Layer:" + str(current_layer))
+
+    def hexdigest(self):
+        return self.__merkle_root_node.hexdigest()
 
 mt = MerkleTree("data")
