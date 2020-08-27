5136566231a037310102b8429d81154116e53acb
Simon Gensler <hallo@simongensler.de>
2020-08-27 10:32:40 +0200

    Add ABI handling

--- src/ethereum/eternal_logfile_ethereum.py
+++ src/ethereum/eternal_logfile_ethereum.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from mt import MerkleTree
 from web3.auto import w3
 
@@ -8,3 +9,10 @@ if not w3.isConnected():
 
 mt = MerkleTree("data")
 hexdigest = mt.hexdigest()
+
+
+abi = ""
+with open("sc_contract_abi.json", "r") as f:
+    abi = json.loads(f.read())
+
+print("ABIT: " + abi)
